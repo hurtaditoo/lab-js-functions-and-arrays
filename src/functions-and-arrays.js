a454f0afdba2924d07027ce7898eb0e0230be4fa
hurtaditoo <al415507@uji.es>
2024-09-28 15:10:39 +0200

    Iteration 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(n1, n2) {
+    if (n1 > n2) {
+        return n1;
+    } else if (n2 > n1) {
+        return n2;
+    } else {
+        return n1;
+    }
+}
 
 
 
